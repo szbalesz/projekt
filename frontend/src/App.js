3f08dabd01525b6ef1989dfb46c0e8fb84498fc4
SzBalesz <szarazb@kkszki.hu>
2025-01-13 20:45:49 +0100

    Profil lekérése adatbázisból + hibaüzenet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,21 @@ import { Box } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [account,SetAccount] = useState({});
+  const [account,SetAccount] = useState({
+    felhasznalonev: "Kijelentkezve",
+    profilkep: ""
+  });
   //http://localhost:5202/felhasznalo
   const onLogin =()=>{
-    fetch("http://localhost:5202/felhasznalo")
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-        SetAccount(data[0]);
-        console.log(data[0])
-    })
+      fetch("http://localhost:5202/felhasznalo")
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+          SetAccount(data[0]);
+          console.log(data[0])
+      })
+      .catch(e => {console.error("HIBA, Nem sikerült a bejelentkezés: ",e)})
   }
   useEffect(() => {
     onLogin();
