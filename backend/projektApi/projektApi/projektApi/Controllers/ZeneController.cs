1691faa33f1977d1d578bd2e99e91645fa6fa8cc
fehert <fehert@kkszki.hu>
2025-01-17 17:14:06 +0100

    Zene gettelés név alapján kész

--- backend/projektApi/projektApi/projektApi/Controllers/ZeneController.cs
+++ backend/projektApi/projektApi/projektApi/Controllers/ZeneController.cs
@@ -29,5 +29,19 @@ namespace projektApi.Controllers
 
             }).ToListAsync());
         }
+
+        [HttpGet("GetMusicByName")]
+        public async Task<ActionResult<Zene>> GetByName(string betu)
+        {
+            return Ok(await projektContext.Zenes.Where(f => (f.Cim.ToLower().Contains(betu.ToLower()) || f.Eloado.ToLower().Contains(betu.ToLower()))).Select(f => new
+            {
+                guid = f.Guid,
+                eloado = f.Eloado,
+                cim = f.Cim,
+                kep = f.Kep,
+
+
+            }).ToListAsync());
+        }
     }
 }
--- backend/projektApi/projektApi/projektApi/obj/Debug/net8.0/projektApi.AssemblyInfo.cs
+++ backend/projektApi/projektApi/projektApi/obj/Debug/net8.0/projektApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projektApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a12b70bd926bb85c3809ac5eaa3fcf147da5bef4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ee41135c2ec0997e814682a5265cd394b0eca99")]
 [assembly: System.Reflection.AssemblyProductAttribute("projektApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projektApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
