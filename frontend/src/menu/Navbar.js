3895a1f83f1a5bf01a8eac1d931407aef24fb307
SzBalesz <szarazb@kkszki.hu>
2025-01-31 11:27:34 +0100

    Navbar logo középre igazítva az összes nézetben

--- frontend/src/menu/Navbar.js
+++ frontend/src/menu/Navbar.js
@@ -13,7 +13,7 @@ export default function Navbar({setIsSidebarOpen, account, isLoggedIn, onLogout}
             <Button display={{base: "none", md:"flex"}} h="50px" fontSize="10px" variant="surface" bg="Background" w="50px" onClick={()=>setIsSidebarOpen(true)}>
                   <Flex justifyContent="center"><LuList/></Flex>
             </Button>
-        <Flex marginLeft="-50px" w="100%" p="0" textAlign="center" justifyContent="center">
+        <Flex marginLeft={{base: "0", md: "-50px"}} w="100%" p="0" textAlign="center" justifyContent="center">
               <Link to={"/"}>
               <Image
               transition="all 0.5s ease-in-out"
