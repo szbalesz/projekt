4d98e29c27f9fa96b6711ff106db8a2a5b2b31d6
CsehiTamas <csehit@kkszki.hu>
2025-02-17 14:04:05 +0100

    Rólunk oldal kinézete frissítve

---
+++ frontend/src/AboutCard.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Box, Button, Card, Image, Text } from "@chakra-ui/react"
+import { LuInstagram } from 'react-icons/lu'
+
+
+export default function AboutCard({item}) {
+  return (
+    <Card.Root m={"5"} maxW="sm" textAlign={"center"} justifyContent={"center"} overflow="hidden">
+      <Box
+      backgroundPosition={"center"}
+      backgroundSize={"cover"}
+      height={"250px"}
+      width={"250px"}
+      backgroundImage={`url(${item.image})`}
+      />
+      <Card.Body gap="2">
+        <Card.Title>{item.name}</Card.Title>
+        <Card.Description>
+          {item.bio}
+        </Card.Description>
+      </Card.Body>
+      <Card.Footer display={"flex"} mx={"auto"} gap="2">
+        <Button variant="solid"><LuInstagram/></Button>
+      </Card.Footer>
+    </Card.Root>
+  )
+}
--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Flex, HStack } from "@chakra-ui/react"
+import { Center } from "@chakra-ui/react"
 import {
   AccordionItem,
   AccordionItemContent,
@@ -7,40 +7,29 @@ import {
   AccordionRoot,
 } from "../components/ui/accordion"
 import { Avatar } from "../components/ui/avatar"
+import AboutCard from '../AboutCard'
 
 export default function About() {
     return (
-        <Flex mx={"auto"} maxWidth={"650px"}> 
-            <AccordionRoot collapsible defaultValue={["b"]}>
-          {items.map((item, index) => (
-            <AccordionItem key={index} value={item.name}>
-              <AccordionItemTrigger>
-                <Avatar shape="rounded" src={item.image} name={item.name} />
-                <HStack>
-                  {item.name}{" "}
-                </HStack>
-              </AccordionItemTrigger>
-              <AccordionItemContent>{item.bio}</AccordionItemContent>
-            </AccordionItem>
-          ))}
-        </AccordionRoot>
-        </Flex>
+        <Center display={"flex"} flexWrap={"wrap"}>
+          {items.map((item,index)=> <AboutCard item={item}/>)}
+        </Center>
       )
 }
 const items = [
     {
       name: "Száraz Bálint Csaba",
-      bio: "asdasd",
+      bio: "Frontend <-> Backend kapcsolat",
       image: "https://yt3.googleusercontent.com/xzvwuor2VIDCJU0MrRcEhYk12Tat4mFafmLWPdmC3AXCp3FS_0ne5WdDzEKP8MTQ9wr4hUBsew=s900-c-k-c0x00ffffff-no-rj",
     },
     {
       name: "Csehi Tamás",
-      bio: "asdasd",
+      bio: "Fronted UI + Dokumentáció",
       image: "https://i.pravatar.cc/150?u=b",
     },
     {
       name: "Fehér Tamás",
-      bio: "asdasdasd",
+      bio: "Backend + Adatbázis",
       image: "https://i.ytimg.com/vi/QDHjHO7jdqg/maxresdefault.jpg",
     },
   ]
