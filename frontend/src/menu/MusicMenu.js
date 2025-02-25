c21526f4f20d93a57b3fb3151f2f4e3adab3e323
SzBalesz <szarazb@kkszki.hu>
2025-02-25 14:14:29 +0100

    Zene műveletek hibái javítva

--- frontend/src/menu/MusicMenu.js
+++ frontend/src/menu/MusicMenu.js
@@ -10,17 +10,24 @@ import AddToPlaylistMenu from '../menu/AddToPlaylistMenu';
 import { LuCircleX, LuList } from "react-icons/lu";
 import DialogAlert from "./DialogAlert";
 import EditMusicWindow from "./EditMusicWindow";
+import Cookies from "js-cookie"
 
-export default function MusicMenu({getMusic,music, setFavorite, isFavorite, musicId, deleteMusic}) {
+export default function MusicMenu({isUploader,getMusic,music, setFavorite, isFavorite, musicId, deleteMusic}) {
+  const token = Cookies.get("token");
   return (
     <MenuRoot>
       <MenuTrigger asChild>
       <Button p={1} m={1} variant="solid"><LuList/></Button> 
       </MenuTrigger>
       <MenuContent>
-        <AddToPlaylistMenu setFavorite={setFavorite} isFavorite={isFavorite} musicId={musicId}/>
-        <EditMusicWindow music={music} getMusic={getMusic}/>
+        {token? 
+        <AddToPlaylistMenu setFavorite={setFavorite} isFavorite={isFavorite} musicId={musicId}/> 
+        : null}
+        {isUploader? 
+        <><EditMusicWindow music={music} getMusic={getMusic}/>
         <DialogAlert openButton={<MenuItem value="torles" color={"red.500"}><LuCircleX/>Törlés</MenuItem>} title={"Biztosan törölni szeretnéd?"} func={deleteMusic} text={"Ez a művelet nem vonható vissza. Ez véglegesen törli a lejátszási listát a rendszerből."} buttontext={"Törlés"}/>
+        </>
+        : null}
       </MenuContent>
     </MenuRoot>
   )
